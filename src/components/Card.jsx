9327978e41b6b935b0f48a071bef419df9cbfd02
Halid Cisse <halidcisse346@gmail.com>
2024-05-06 00:21:05 +0200

    Update styles and dependencies

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -15,18 +15,25 @@ function Card({
     const stringWithoutNumbers = nomeImmagineCamera.replace(/\d+/g, "");
     setImgNewPath(stringWithoutNumbers);
 
-    if (descrizione.length > 75) {
-      setNewDescrizione(descrizione.slice(0, 75) + "...");
+    if (descrizione.length > 70) {
+      const lastSpaceIndex = descrizione.lastIndexOf(" ", 70);
+      const newDescrizione =
+        lastSpaceIndex === -1
+          ? descrizione.slice(0, 70)
+          : descrizione.slice(0, lastSpaceIndex);
+      setNewDescrizione(newDescrizione + "...");
+    } else {
+      setNewDescrizione(descrizione);
     }
   }, [nomeImmagineCamera]);
 
   return (
-    <div className=" z-40 border-[1px] border-[#7E8794] flex flex-col justify-start items-center w-[15rem] rounded-md">
+    <div className=" z-40 border-[1px] border-[#7E8794] cursor-pointer flex flex-col justify-start items-center w-[15rem] rounded-md hover:scale-[.95] transition-transform duration-300 ease-in-out ">
       <img
         src={`src/progettoInfoImage/${imgNewPath.trim()}/${nomeImmagineCamera.trim()}.jpg`}
-        className="rounded-t-md w-full"
+        className="rounded-t-md w-full h-[10rem] object-cover"
         loading="lazy"
-        alt="albaSuite1"
+        alt={nomeImmagineCamera.trim()}
       />
 
       <div className="p-2 px flex flex-col justify-center items-start gap-2">
--- src/pages/Camere.jsx
+++ src/pages/Camere.jsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import Card from "../components/Card";
 import { ListFilter } from "lucide-react";
+import { Dialog } from "primereact/dialog";
 
 function Camere() {
   const [camere, setCamere] = useState([]);
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
 
   useEffect(() => {
     fetch("http://localhost/progetto-fullstack-informatica-quinta/camere.php")
@@ -17,10 +19,62 @@ function Camere() {
   return (
     <div className="h-full w-full">
       <div className="h-[15%] w-full flex justify-start items-center">
-        <button className="border-none outline-none flex justify-center items-center gap-1 ml-5">
+        <button
+          onClick={() => setIsDialogOpen(true)}
+          className="border-none outline-none flex justify-center items-center gap-1 ml-5"
+        >
           <ListFilter color="#7E8794" />
           <span className="font-bold text-[#7E8794]">Filtri</span>
         </button>
+        <Dialog
+          visible={isDialogOpen}
+          className="w-[40vw] h-[50vh] flex flex-col justify-center items-center bg-white shadow-[0px_0px_10px_6px_#00000024] rounded-md px-5"
+          onHide={() => setIsDialogOpen}
+        >
+          <form className="w-full" action="" method="post">
+            <div className="w-full flex justify-between items-center">
+              <p className="w-[40%] font-bold">Tipo camera:</p>
+              <div className="w-[60%] flex justify-start items-center gap-7">
+                <div className="flex justify-center items-center gap-2">
+                  <input
+                    type="checkbox"
+                    id="singola"
+                    name="singola"
+                    value="1"
+                  />
+                  <label for="singola">Singola</label>
+                </div>
+
+                <div className="flex justify-center items-center gap-2">
+                  <input type="checkbox" id="doppia" name="doppia" value="1" />
+                  <label for="doppia">Doppia</label>
+                </div>
+
+                <div className="flex justify-center items-center gap-2">
+                  <input type="checkbox" id="suite" name="suite" value="1" />
+                  <label for="suite">Suite</label>
+                </div>
+              </div>
+            </div>
+            <div className="w-full flex justify-between items-center">
+              <p className="w-[40%] font-bold">Posti letto:</p>
+              <div className="w-[60%] flex justify-start items-center">
+                <input
+                  className="w-[50%] h-4 border-[1.5px] border-[#0B76B7] rounded-sm py-2"
+                  type="number"
+                  name="postiLetto"
+                  id="postiLetto"
+                  min={1}
+                  max={7}
+                />
+              </div>
+            </div>
+
+            <button type="button" onClick={() => setIsDialogOpen(false)}>
+              Chiudi
+            </button>
+          </form>
+        </Dialog>
       </div>
       <div className="h-[85%] ml-5 w-full flex items-start justify-start flex-wrap gap-x-20 gap-y-3">
         {camere.map((camera) => (
@@ -29,7 +83,7 @@ function Camere() {
             nomeCamera={camera.nomeCamera}
             tipoCamera={camera.tipoCamera}
             postiLetto={`${camera.postiLetto} letto`}
-            prezzo={`${camera.prezzo}$/N`}
+            prezzo={`${camera.prezzo}€/N`}
             descrizione={camera.descrizione}
             nomeImmagineCamera={camera.nomeImmagineCamera}
           />
