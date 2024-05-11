0228283b22e356d37422687923766564e45e296b
Halid Cisse <halidcisse346@gmail.com>
2024-05-11 16:32:12 +0200

    chore: Update Prenotazioni page to fetch and display user bookings

--- src/pages/Prenotazioni.jsx
+++ src/pages/Prenotazioni.jsx
@@ -1,9 +1,40 @@
 import React, { useState, useEffect } from "react";
 
 function Prenotazioni() {
+  const [utenteID, setUtenteID] = useState(localStorage.getItem("userId"));
+  const [riepilogoPrenotazioni, setRiepilogoPrenotazioni] = useState([]);
+
+  useEffect(() => {
+    getUtentePrenotazioniData();
+  }, []);
+
+  const getUtentePrenotazioniData = () => {
+    fetch(
+      `http://localhost/progetto-fullstack-informatica-quinta/prenotazioni.php?utenteID=${utenteID}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setRiepilogoPrenotazioni(data);
+      })
+      .catch((error) =>
+        console.error(
+          "Errore durante il recupero dell'immagine della camera:",
+          error
+        )
+      );
+  };
   return (
-    <div className="text-black">
-      <h1>Prenotazioni</h1>
+    <div className="h-full w-full overflow-auto flex flex-col justify-start items-start">
+      <div className="h-[5rem] w-full flex justify-start items-center">
+        <h1 className="text-[#0B76B7] text-xl font-bold ml-5">
+          Riepilogo delle prenotazioni dell’utente
+        </h1>
+      </div>
+
+      <table>
+        <tr></tr>
+      </table>
     </div>
   );
 }
